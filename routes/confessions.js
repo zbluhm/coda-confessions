4a72c5a240873a9f91dacb3681e89cbdce334781
Zach <zacharybluhm@capitalone.com>
2017-09-22 15:57:08 -0400

    post route done

--- routes/confessions.js
+++ routes/confessions.js
@@ -17,6 +17,20 @@ router.get('/', function(req, res, next) {
 router.post('/', function (req, res, next) {
     //create new confession
 
+    db.user.findOrCreate({
+        where: {name: req.body.name}
+    }).spread((user, create) => {
+        db.confession.create({
+            message: req.body.message,
+            userId: user.id
+        }).then(result => {
+            if (!result) throw Error();
+            res.status(200).json(result);
+        }).catch(function (error) {
+            res.json(error.toString())
+        })
+    })
+
 })
 
 module.exports = router;
