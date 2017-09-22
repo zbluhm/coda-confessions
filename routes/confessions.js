628bf3f2ab82cdf1bd184ae5eea60e5cb216a4e8
zbluhm <zbluhm@users.noreply.github.com>
2017-09-22 15:01:03 -0400

    Revert "added initial css"

--- routes/confessions.js
+++ routes/confessions.js
@@ -5,14 +5,13 @@ var db = require('../models');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     //return all confessions
-    // db.confession.findAll().then(function(confessions) {
-    //     if (!confessions) throw Error();
-    //     res.status(200).json(confessions);
-    // })
-    //   .catch(function(error) {
-    //       res.json(error);
-    //   });
-    res.send('coderGirl')
+    db.confession.findAll().then(function(confessions) {
+        if (!confessions) throw Error();
+        res.status(200).json(confessions);
+    })
+      .catch(function(error) {
+          res.json(error);
+      });
 });
 
 router.post('/', function (req, res, next) {
